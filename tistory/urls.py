947fe8d9c37b9cc3bd9befdf46d465e3b432a880
igoo-Y <yang_ig@naver.com>
2021-06-01 23:41:37 +0900

    delete

--- tistory/urls.py
+++ tistory/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('create/', create, name="create"),
     path('edit/<str:id>', edit, name="edit"),
     path('update/<str:id>', update, name="update"),
+    path('delete/<str:id>', delete, name="delete"),
 ]
 

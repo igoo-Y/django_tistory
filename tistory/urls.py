538308d23721e54f3bc2dee0b0ee0cf3484df430
igoo-Y <yang_ig@naver.com>
2021-06-01 22:56:41 +0900

    edit

--- tistory/urls.py
+++ tistory/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     path('<str:id>', detail, name="detail"),
     path('new/', new, name="new"),
     path('create/', create, name="create"),
+    path('edit/<str:id>', edit, name="edit"),
 ]
 

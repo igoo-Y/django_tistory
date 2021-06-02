e977e561173c22c35cf8b8948d8217007d0bafca
igoo-Y <yang_ig@naver.com>
2021-06-02 13:20:01 +0900

    templates extend

--- tistory/settings.py
+++ tistory/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'tistory.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['tistory/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
